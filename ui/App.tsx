b8ffef78f64ea1574a241a6db91d3416f77a6534
Andrew Gilliland <andrewpgilliland@gmail.com>
2025-01-14 17:03:43 -0600

    feat: initial commit

---
+++ ui/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  // Make Fetch Request from Fastify Server API endpoint
+  // Return PDF file
+  // Display PDF file in browser
+
+  return (
+    <div>
+      <button>View PDF</button>
+    </div>
+  );
+}
+
+export default App;
