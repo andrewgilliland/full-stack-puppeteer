a37e67ae0e82bd3c584146854d9b96905cc3f510
Andrew Gilliland <andrewpgilliland@gmail.com>
2025-01-23 14:49:24 -0600

    feat: adds a download and img on the client

--- ui/App.tsx
+++ ui/App.tsx
@@ -1,9 +1,14 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { getPDF } from "./utils";
 
 function App() {
   const [loading, setLoading] = useState(false);
+  const [pdf, setPDF] = useState("");
+
+  useEffect(() => {
+    console.log("pdf: ", pdf);
+  }, [pdf]);
 
   return (
     <div>
@@ -11,12 +16,33 @@ function App() {
         disabled={loading}
         onClick={async () => {
           setLoading(true);
-          await getPDF();
+          const result = await getPDF();
+
+          setPDF(result);
+
           setLoading(false);
         }}
       >
         {loading ? "Loading..." : "Generate PDF"}
       </button>
+
+      {pdf && (
+        <div
+          style={{ marginTop: 32, display: "flex", flexDirection: "column" }}
+        >
+          <button>
+            <a href={pdf} download="file.pdf">
+              Download PDF
+            </a>
+          </button>
+          <img
+            style={{ border: "1px solid red", marginTop: 16 }}
+            src={pdf}
+            width="250"
+            height="200"
+          />
+        </div>
+      )}
     </div>
   );
 }
--- ui/utils/index.ts
+++ ui/utils/index.ts
@@ -1,8 +1,16 @@
 const baseURL = "http://localhost:3000";
 
+/** Return PDF as image  */
 export const getPDF = async () => {
   const response = await fetch(`${baseURL}/pdf`);
   const data = await response.blob();
+
+  console.log("PDF received");
+
   const file = window.URL.createObjectURL(data);
-  window.open(file);
+
+  const src = file.replace("blob:", "");
+
+  console.log(file);
+  return src;
 };
