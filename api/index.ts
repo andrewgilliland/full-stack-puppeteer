6316378a6c7d6c71661dacfa305fcef2f54c8a0f
Andrew Gilliland <andrewpgilliland@gmail.com>
2025-02-05 15:13:48 -0600

    feat: adds getHeaderTemplate function and formats pdf

--- api/index.ts
+++ api/index.ts
@@ -1,34 +1,6 @@
 import Fastify from "fastify";
 import cors from "@fastify/cors";
-import puppeteer from "puppeteer";
-
-export const getPDF = async (url: string) => {
-  console.log("Request received");
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto(url);
-
-  const dom = await page.$eval("div.testimonials", (elemant) => {
-    return elemant.innerHTML;
-  });
-
-  await page.setContent(dom);
-
-  const pdf = await page.pdf();
-
-  await browser.close();
-  console.log("PDF generated");
-  return pdf;
-};
-
-export const getScreenshot = async (url: string) => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto(url);
-  const screenshot = await page.screenshot();
-  await browser.close();
-  return screenshot;
-};
+import { getPDF } from "./puppeteer/index.ts";
 
 const fastify = Fastify({});
 
@@ -44,12 +16,6 @@ fastify.get("/pdf", async function handler() {
   const pdf = await getPDF("https://www.freecodecamp.org/");
 
   return pdf;
-});
-
-fastify.get("/screenshot", async function handler() {
-  const screenshot = await getScreenshot("https://www.freecodecamp.org/");
-
-  return screenshot;
 });
 
 // Run the server!
--- api/puppeteer/index.ts
+++ api/puppeteer/index.ts
@@ -1,10 +1,34 @@
 import puppeteer from "puppeteer";
+import fs from "fs";
+
+const getHeaderTemplate = () => {
+  const html = fs.readFileSync("./api/puppeteer/header-template.html", "utf8");
+
+  console.log(html);
+
+  return html;
+};
 
 export const getPDF = async (url: string) => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(url);
-  const pdf = await page.pdf();
+
+  const pdf = await page.pdf({
+    path: "report.pdf",
+    displayHeaderFooter: true,
+    headerTemplate: getHeaderTemplate(),
+    footerTemplate: "<p>Report Footer</p>",
+    printBackground: true,
+    format: "A4",
+    margin: {
+      top: ".5in",
+      bottom: ".5in",
+      left: ".5in",
+      right: ".5in",
+    },
+  });
+
   await browser.close();
   console.log("PDF generated");
 
